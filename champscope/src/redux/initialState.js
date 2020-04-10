04b096772cc75442cd8d4d5706fd8f3a2a1935d4
Yuke Zheng <yukezheng@yukes-mbp-2.lan>
2020-04-10 16:32:39 -0400

    fixed toggle states

--- champscope/src/redux/initialState.js
+++ champscope/src/redux/initialState.js
@@ -5,7 +5,16 @@
 const initialState = {
     LOADING: true,
     START_SCENE: false,
-    MENU_INTERACTION: {},
+    MENU_INTERACTION: {
+        scenarioSwitch: false,
+        parks: true,
+        culturalBuildings: true,
+        cars: true,
+        bicycles: true,
+        pedestrians: true,
+        animateCamera: true,
+        quality: true,
+    },
 };
 
 export default initialState;
