e725bb3203efab5207af4d4d77f7e5557f2e94e4
Ariel Noyman <noyman@mit.edu>
2020-04-19 19:03:35 -0400

    Updates

---
+++ precache-manifest.d7ab4d87788711f1ee6518afc347aa6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd71f2daa8bc21aad3579dd1e88ee0a",
+    "url": "/CSL_Paris/index.html"
+  },
+  {
+    "revision": "5c1aacddd0b38ec8e2b5",
+    "url": "/CSL_Paris/static/css/main.340f0968.chunk.css"
+  },
+  {
+    "revision": "d6a05531a4090954381d",
+    "url": "/CSL_Paris/static/js/2.10c5bde8.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/CSL_Paris/static/js/2.10c5bde8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1aacddd0b38ec8e2b5",
+    "url": "/CSL_Paris/static/js/main.e9683f7b.chunk.js"
+  },
+  {
+    "revision": "67516c860defb158c450",
+    "url": "/CSL_Paris/static/js/runtime-main.19db85cd.js"
+  }
+]);
